03a2c1e52dd9a896fca71112417f4815e34fe0bd
maheshDivekar <42762536+maheshDivekar@users.noreply.github.com>
2024-05-30 10:48:30 +0530

    Add project files.

---
+++ HashingSaltTest/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using HashingSaltTest;
+using System.Text;
+
+HashingAndSaltingPasswords saltTest = new HashingAndSaltingPasswords();
+var haspassword =saltTest.HashPasword("MaheshDivekar", out var salt);
+var saltstring = Convert.ToBase64String(salt);
+bool ishashmatch = saltTest.VerifyPassword("MaheshDivekar", haspassword, salt);
+bool ishashmatch1 = saltTest.VerifyPassword("MaheshDivekar", haspassword, Convert.FromBase64String(saltstring));
+Console.WriteLine("Hello, World!");
